ddd24b4d672661b7828be65b81d43f795d2bc680
SaltLakeAtrocity <rjv2749@email.vccs.edu>
2016-06-20 21:08:11 -0400

    Add files via upload

--- Programming Assignment Two.cpp
+++ Programming Assignment Two.cpp
@@ -11,15 +11,18 @@
 #include <string>
 using namespace std;
 
-
+ifstream files;
+	ofstream outfile;
+	string 	filename, lastname, errormessageM, errormessageP;
+	char marital;
+	int children;
+	float income, pension, deductions, taxincome, taxowed, exemptions;
+	void tax_calc();
 
 int main()
 {
-	ifstream files;
-	ofstream outfile;
-	string 	filename, lastname, marital;
-	int children;
-	float income, pension;
+	errormessageM = " ";
+	errormessageP = " ";
 	
 	cout << "Please tell me your file name.\n";
 	cin >> filename;
@@ -27,36 +30,47 @@ int main()
 	outfile.open("Outputfile.txt"); 	
 			if (files.is_open())
 			{
+				
+				cout	<< "=========================================================================================================\n";
+				cout	<< setw(10) << left << "Name " << setw(10) << left << "Gross-Income "
+						<< setw(10) << left << "Exemptions " << setw(10) << left << "Pension "
+						<< setw(10) << left << "Taxable-Income " << setw(10) << left << "Tax-Owed: "
+						<< setw(10) << left << "Notes: \n";
+				cout	<< "=========================================================================================================\n";		
+				
+				outfile	<< "=========================================================================================================\n";
+				outfile	<< setw(9) << left << "Name " << setw(9) << left << "Gross-Income "
+						<< setw(9) << left << "Exemptions " << setw(8) << left << "Pension "
+						<< setw(9) << left << "Taxable-Income " << setw(9) << left << "Tax-Owed: "
+						<< setw(8) << left << "Notes: \n";
+				outfile	<< "=========================================================================================================\n";
 				do
 				{
 					files >> lastname >> marital >> children >> income >> pension;
+					tax_calc();
+//********************************************************************************************************************					
 					if (files)
-					{
-						cout << fixed << showpoint << setprecision(2);
-						cout	<< "======================================================================\n";
-						cout 	<< "Last Name: " 
-										<< setw(9) << left << lastname << " ~~ " 
-								<< "Marital Status: " 
-										<< setw(4) << left << marital << " ~~ "
-								<< "Number of Children: " 
-										<< setw(4) << left << children  
-								<< "Yearly Income: $" 
-										<< setw(10) << left << income 
-								<< " ~~ Pension Contribution: " 
-										<< setw(4) << left << pension * 100 << "%" << "\n";
-						outfile 	<< fixed << showpoint << setprecision(2);
-						outfile		<< "======================================================================\n";
-						outfile 	<< "Last Name: " 
-										<< setw(9) << left << lastname << " ~~ " 
-									<< "Marital Status: " 
-										<< setw(4) << left << marital << " ~~ "
-									<< "Number of Children: " 
-										<< setw(4) << left << children 
-									<< "Yearly Income: $" 
-										<< setw(10) << left << income 
-									<< " ~~ Pension Contribution: " 
-										<< setw(4) << left << pension * 100 << "%" << "\n";	
+					{	
+						cout 	<< fixed << showpoint << setprecision(2);
+						cout 	<< setw(7)  << left << lastname << "  "   
+								<< setw(11) << left << income << "  "
+								<< setw(9)  << left << exemptions << "  "
+								<< setw(4)  << left << pension * 100 << setw(4) << left << "% "
+								<< setw(13) << left << taxincome << "  "
+								<< setw(8)  << left << taxowed << "  "
+								<< setw(15) << left << errormessageM << "  "
+								<< setw(15) << left << errormessageP << "\n";
+						outfile	<< fixed << showpoint << setprecision(2);
+						outfile << setw(7)  << left << lastname << "  "   
+								<< setw(11) << left << income << "  "
+								<< setw(9)  << left << exemptions << "  "
+								<< setw(4)  << left << pension * 100 << setw(4) << left << "% "
+								<< setw(13) << left << taxincome << "  "
+								<< setw(8)  << left << taxowed << "  "
+								<< setw(15) << left << errormessageM << "  "
+								<< setw(15) << left << errormessageP << "\n";	
 					}
+//********************************************************************************************************************
 				} 	while (files);
 				
 			}
@@ -80,4 +94,69 @@ int main()
 				cout << "Could not save output file.\n";
 			}
 return 0;
+}
+//********************************************************************************************************************
+//********************************************************************************************************************
+void tax_calc()
+{
+	//ifstream files;
+	//ofstream outfile;
+	//string 	filename, lastname, errormessageM, errormessageP;
+	//char marital;
+	//int children;
+	//float income, pension, deductions, taxincome, taxowed, exemptions;
+	//errormessageM = " ";
+	//errormessageP = " ";
+//********************************************************************************************************************	
+	switch (marital)
+	{
+		case 'M':
+			{
+				exemptions = 6000 + (700 * 2) + (700 * children);
+				errormessageM = " ";
+				break;
+			}
+		case 'S':
+			{
+				exemptions = 3000 + (700) + (700 * children);
+				errormessageM = " ";
+				break;
+			}
+		default:
+			{
+				exemptions = 3000 + (700) + (700 * children);
+				errormessageM = "Invalid status.";
+				break;
+			}
+	}
+//********************************************************************************************************************	
+	if (pension >= .06)
+	{
+		errormessageP = "Invalid pension.";
+		pension = 0.05;
+	}
+	
+	if (pension <= .05)
+	{
+		errormessageP = " ";
+	}
+//********************************************************************************************************************
+	deductions = pension * income;
+	taxincome = (income - exemptions) - deductions;
+//********************************************************************************************************************	
+	if (taxincome <= 20000.00)
+	{
+		taxowed = (0.15 * taxincome);
+	}
+	
+	if (taxincome >= 20000.01 && taxincome <= 50000.00)
+	{
+		taxowed = (0.25 * taxincome) + 2250;
+	}
+	
+	if (taxincome >= 50000.01)
+	{
+		taxowed = (0.35 * taxincome) + 8460;
+	}
+//********************************************************************************************************************
 }
